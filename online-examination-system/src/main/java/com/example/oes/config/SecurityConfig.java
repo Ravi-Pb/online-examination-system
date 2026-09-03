f651f8c25c8129a9babb0e604e6084c5c5de0a24
Ravi_Pb <purbeyravi27@gmail.com>
2026-09-04 01:04:38 +0530

    added varcel link in cors list / linking of frontend to backend

--- online-examination-system/src/main/java/com/example/oes/config/SecurityConfig.java
+++ online-examination-system/src/main/java/com/example/oes/config/SecurityConfig.java
@@ -243,7 +243,9 @@ public class SecurityConfig {
 
         // React development server
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("http://localhost:5173",
+                        "https://online-examination-system-ravi-pb.vercel.app/"
+                        )
         );
 
         // HTTP methods used by our frontend
