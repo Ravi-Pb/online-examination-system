0bbadb94be0995958bcf2b4cc1229982b24e127b
Ravi_Pb <purbeyravi27@gmail.com>
2026-09-04 15:59:38 +0530

    final commit.4.-updated backend url

--- online-examination-system/src/main/java/com/example/oes/config/SecurityConfig.java
+++ online-examination-system/src/main/java/com/example/oes/config/SecurityConfig.java
@@ -244,7 +244,8 @@ public class SecurityConfig {
         // React development server
         configuration.setAllowedOrigins(
                 List.of("http://localhost:5173",
-                        "https://online-examination-system-ravi-pb.vercel.app"
+//                        "https://online-examination-system-ravi-pb.vercel.app",
+                        "https://online-examination-system-r40g4po6u-ravi-pb.vercel.app"
                         )
         );
 
